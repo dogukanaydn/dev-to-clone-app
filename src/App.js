d3da4d46caf072d863b3643d3e2752a32e0dc9f6
Doğukan Aydın <ddogukanaydin@gmail.com>
2020-09-12 21:41:48 +0300

    Sidebar part added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
 import "./index.css";
 import Navbar from "./components/Navbar";
+import SidebarProfile from "./components/SidebarProfile";
+import SidebarContent from "./components/SidebarContent";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <div className="wrapper">
+        <div className="one">
+          <SidebarProfile />
+          <SidebarContent />
+          <SidebarContent />
+          <SidebarContent />
+          <SidebarContent />
+          <SidebarContent />
+          <SidebarContent />
+        </div>
+        <div className="two">
+          <p>Two</p>
+        </div>
+        <div className="three">
+          <p>Three</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
